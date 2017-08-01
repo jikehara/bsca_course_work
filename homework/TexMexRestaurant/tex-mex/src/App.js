c1e403ef65488b4a6893e09f3277ec0ac8b58a96
root <root@Naptop.localdomain>
2017-08-01 12:35:44 -0700

    Working Tex Mex restaurant with spice tolerance implemented, but no user input yet.

--- homework/TexMexRestaurant/tex-mex/src/App.js
+++ homework/TexMexRestaurant/tex-mex/src/App.js
@@ -2,12 +2,21 @@ import React, { Component } from 'react';
 import {AboutRestaurant, FoodItems, Data} from './Components';
 
 class App extends Component {
+
+  state = {
+    spiceTolerance: 10
+  }
+
+  updateSpiceTolerance = (event) => {
+    this.setState({spiceTolerance: event.target.value})
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hello from App!</h1>
         <AboutRestaurant name={Data.title} address={Data.address} phone={Data.phone} />
-        <FoodItems />
+        <FoodItems items={Data.items} spicy={this.state.spiceTolerance}/>
       </div>
     );
   }
--- homework/TexMexRestaurant/tex-mex/src/Components/FoodItems.js
+++ homework/TexMexRestaurant/tex-mex/src/Components/FoodItems.js
@@ -1,9 +1,38 @@
 import React from 'react';
 
-const FoodItems = () => {
+const FoodItems = (props) => {
+
+  // const MappedFood = props.items.map((food, index) => {
+  //   return (
+  //     <div key={index} className="food-cards">
+  //       <h3>{food.name} for {food.price} dollars</h3>
+  //       <p>category: {food.category} </p>
+  //       <p>spicy level: {food.spiceLevel}</p>
+  //       <img src={food.img} alt="dummy alt text" />
+  //     </div>
+  //   )
+  // })
+
+  const FilteredBySpice = props.items.filter((items) => {
+    return items.spiceLevel < props.spicy
+  }).map((food, index) => {
+    return (
+      <div key={index} className="food-cards">
+        <h3>{food.name} for {food.price} dollars</h3>
+        <p>category: {food.category} </p>
+        <p>spicy level: {food.spiceLevel}</p>
+        <img src={food.img} alt="dummy alt text" />
+      </div>
+    )
+  })
+
+
   return (
     <div>
       <h1>Hello from Food Items!</h1>
+      <ul className="food-list">
+        {FilteredBySpice}
+      </ul>
     </div>
   )
 }
