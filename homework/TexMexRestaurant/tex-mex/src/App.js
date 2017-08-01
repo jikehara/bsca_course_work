6a711f9c8dea33cf71d8eb881a0c919814fc61e4
root <root@Naptop.localdomain>
2017-08-01 13:14:04 -0700

    Added a form to allow users to update the spice tolerance level, it works temporarily.

--- homework/TexMexRestaurant/tex-mex/src/App.js
+++ homework/TexMexRestaurant/tex-mex/src/App.js
@@ -4,7 +4,7 @@ import {AboutRestaurant, FoodItems, Data} from './Components';
 class App extends Component {
 
   state = {
-    spiceTolerance: 10
+    spiceTolerance: 5
   }
 
   updateSpiceTolerance = (event) => {
@@ -16,6 +16,12 @@ class App extends Component {
       <div className="App">
         <h1>Hello from App!</h1>
         <AboutRestaurant name={Data.title} address={Data.address} phone={Data.phone} />
+
+        <h3>Select your spice tolerance! (current is {this.state.spiceTolerance})</h3>
+        <form onSubmit={this.handleFormSubmit}>
+        <input onChange={this.updateSpiceTolerance} />
+        </form>
+
         <FoodItems items={Data.items} spicy={this.state.spiceTolerance}/>
       </div>
     );
