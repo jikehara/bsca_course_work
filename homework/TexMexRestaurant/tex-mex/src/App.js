c33de3a347c56b227f762a245b951d272cc09d43
root <root@Naptop.localdomain>
2017-08-02 08:03:19 -0700

    Buttons work but the functions to set state are hard coded. Fix this later.

--- homework/TexMexRestaurant/tex-mex/src/App.js
+++ homework/TexMexRestaurant/tex-mex/src/App.js
@@ -12,13 +12,26 @@ class App extends Component {
     this.setState({spiceTolerance: event.target.value})
   }
 
-  updateFoodCategory = (props) => {
+  // never ever do this again
+  updateCategoryAppetizers = () => {
     // four categories: Amazing Appetizers, Delicious Desserts, Incredible Entrees, Spicy Specials
-    this.setState({category: props.newCategory})
+    this.setState({category: "Amazing Appetizers"})
   }
-
-
-
+  // never ever do this again
+  updateCategoryDesserts = () => {
+    // four categories: Amazing Appetizers, Delicious Desserts, Incredible Entrees, Spicy Specials
+    this.setState({category: "Delicious Desserts"})
+  }
+  // never ever do this again
+  updateCategoryEntrees = () => {
+    // four categories: Amazing Appetizers, Delicious Desserts, Incredible Entrees, Spicy Specials
+    this.setState({category: "Incredible Entrees"})
+  }
+  // never ever do this again
+  updateCategorySpecials= () => {
+    // four categories: Amazing Appetizers, Delicious Desserts, Incredible Entrees, Spicy Specials
+    this.setState({category: "Spicy Specials"})
+  }
 
   render() {
     return (
@@ -31,13 +44,13 @@ class App extends Component {
         <input onChange={this.updateSpiceTolerance} />
         </form>
 
-        {/* some broken code, but right idea 
+        {/* some naughty code, but right idea
           find a way to make buttons update state*/}
         <h3> Select a food category! (Current is {this.state.category})</h3>
-        <button onClick={()=> console.log("Amazing Appetizers")}>Amazing Appetizers</button>
-        <button onClick={()=> console.log("Delicious Desserts")}>Delicious Desserts</button>
-        <button onClick={()=> console.log("Incredible Entrees")}>Incredible Entrees</button>
-        <button onClick={()=> console.log("Spicy Specials")}>Spicy Specials</button>
+        <button onClick={this.updateCategoryAppetizers}>Amazing Appetizers</button>
+        <button onClick={this.updateCategoryDesserts}>Delicious Desserts</button>
+        <button onClick={this.updateCategoryEntrees}>Incredible Entrees</button>
+        <button onClick={this.updateCategorySpecials}>Spicy Specials</button>
 
         <FoodItems items={Data.items} spicy={this.state.spiceTolerance} cat={this.state.category}/>
       </div>
---
+++ homework/TexMexRestaurant/tex-mex/src/Components/CategoryButton.js
@@ -0,0 +1,13 @@
+// import React from 'react';
+//
+// const CategoryButton = (props) => {
+//
+//
+//   return (
+//     <div className='category-button'>
+//       <button
+//     </div>
+//   )
+// }
+//
+// export default CategoryButton;
